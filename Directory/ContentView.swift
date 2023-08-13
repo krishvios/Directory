f6dac01f2bf7d7d55734a3a9571b77464d7b7b09
Vivek Amirapu <krishiosdev005@gmail.com>
2023-08-13 09:01:55 +0100

    Initial Commit

---
+++ Directory/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Directory
+//
+//  Created by ANSK Vivek on 13/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Directory/DirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectoryApp.swift
+//  Directory
+//
+//  Created by ANSK Vivek on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
